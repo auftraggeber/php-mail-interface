fb2ea28b4ffbac4adc4b4a100b1526adcea27cda
Jonas Eddy Langner <Jonas.Langner.kuz@fh-zwickau.de>
2023-10-12 13:40:16 +0200

    Added auth.

--- mail.php
+++ mail.php
@@ -4,12 +4,16 @@ use PHPMailer\PHPMailer\SMTP;
 
 ini_set('display_errors', 0);
 
+require_once 'setup.php';
 require_once 'fetch_data.php';
 require_once 'phpmailer/PHPMailer.php';
 require_once 'phpmailer/Exception.php';
 require_once 'phpmailer/SMTP.php';
 require_once 'cache.php';
 require_once 'settings.php';
+require_once 'auth.php';
+
+auth_this_http_request();
 
 $_POST = DefaultSettings::applyOn($_POST);
 
